b6ba89b1bc3aaadd3dc371cb1776323f19dc9acb
Sheehan, Philip <Philip.Sheehan@emc.com>
2023-11-02 09:50:46 +0000

    readthedocs failing
    readthedocs failing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,9 +48,9 @@ todo_include_todos = False
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = [
-    #'sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosectionlabel'
-#]
+extensions = [
+    'sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosectionlabel'
+]
 
 autosectionlabel_prefix_document = True
 
@@ -63,12 +63,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 # Add any paths that contain templates here, relative to this directory.
-#templates_path = ['rtd_theme', ]
+templates_path = ['rtd_theme', ]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
